2ae9a4ce89102b46e2bff4413600122194f0b2f5
Benjamin Kinga <somethingscripted@gmail.com>
2024-12-28 02:56:32 -0500

    simplify the interface.
    This just removes a ton of unnecessary indirections that were making it harder
    to understand what I was even doing. Completely not needed.
    
    Now there's only a few abstractions (for now): `tick`, `run`, and `new`.
    
    Hopefully I can keep it that way.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use std::error::Error;
 use std::ops::{Deref, DerefMut};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let file_name = "./src/IBM Logo.ch8";
-    let rom = std::fs::read(file_name).unwrap();
-    let mut chip_8 = Chip8Engine::new(&rom); // TODO(ben): does having the instance itself be
-                                             // mutable make sense?
-
-    chip_8.run()?;
-    println!("{:?}", chip_8.display);
-
-    Ok(())
-}
-
 const DISPLAY_WIDTH: usize = 64;
 const DISPLAY_HEIGHT: usize = 32;
-const DISPLAY_SIZE: usize = DISPLAY_WIDTH * DISPLAY_HEIGHT;
+const FONT_SET: [i32; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
 
 // TODO(ben): for simplicity, let's use vectors for arrays and later optimize for stack-allocated
 // arrays once I figure out how to use them properly...
@@ -35,31 +40,24 @@ struct Chip8Engine {
 
 #[derive(Debug)]
 struct Display {
-    screen: [bool; DISPLAY_WIDTH * DISPLAY_HEIGHT], // TODO(ben): use constants for these values.
+    screen: [u8; DISPLAY_WIDTH / 8 * DISPLAY_HEIGHT],
     width: usize,
     height: usize,
 }
+
+// TODO(ben): Question this. Is this an abstraction or an indirection?
 impl Display {
     fn new() -> Self {
         Display {
-            screen: [false; DISPLAY_WIDTH * DISPLAY_HEIGHT],
-            width: DISPLAY_WIDTH,
+            screen: [0x0; DISPLAY_WIDTH / 8 * DISPLAY_HEIGHT],
+            width: DISPLAY_WIDTH / 8,
             height: DISPLAY_HEIGHT,
         }
     }
-
-    // [
-    //     0,0,0,0,0,
-    //     0,0,0,0,0,
-    //     0,0,0,0,0,
-    //     0,0,0,0,0,
-    //     0,0,0,0,0
-    // ]
-
     // TODO(ben): where should modulo operation be? In the callsite or in the function?
     /// write_pixel writes a pixel value at the point x, y, returning the new value of that pixel.
     /// If that point does not exist in the display, it will return none.
-    fn write_pixel(&mut self, x: usize, y: usize, value: bool) -> Option<bool> {
+    fn write_pixel(&mut self, x: usize, y: usize, value: u8) -> Option<u8> {
         if x >= self.width || y >= self.height {
             None
         } else {
@@ -70,7 +68,7 @@ impl Display {
 
     /// get_pixel returns a pixel value at the point x, y. If that point does not exist in the
     /// display, it will return none.
-    fn get_pixel(&self, x: usize, y: usize) -> Option<bool> {
+    fn get_pixel(&self, x: usize, y: usize) -> Option<u8> {
         if x >= self.width || y >= self.height {
             None
         } else {
@@ -80,7 +78,7 @@ impl Display {
 }
 
 impl Deref for Display {
-    type Target = [bool; DISPLAY_SIZE];
+    type Target = [u8; DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
 
     fn deref(&self) -> &Self::Target {
         &self.screen
@@ -94,27 +92,17 @@ impl DerefMut for Display {
 }
 
 #[derive(Debug)]
-enum Instructions {
-    Clear,
-    Jump,
-    SetVX,
-    AddVX,
-    SetI,
-    Draw,
-}
-
-#[derive(Debug)]
 struct Opcode {
     x: u8, //TODO(Ben): this is exploration. Is usize better or u16? u8? boolean?
     y: u8,
     n: u8,
-    nn: u8,
-    nnn: u16,
-    instruction: Instructions,
+    kk: u8,
+    addr: u16,
 }
 
 impl Chip8Engine {
-    pub fn new(rom: &Vec<u8>) -> Self {
+    pub fn new(rom_file: &str) -> Result<Self, Box<::std::io::Error>> {
+        let rom = std::fs::read(rom_file).unwrap();
         // initialize font_set
         let mut memory = vec![0; 2096];
         for (i, &font_byte) in FONT_SET.iter().enumerate() {
@@ -125,7 +113,7 @@ impl Chip8Engine {
             memory[0x200 + i] = *byte;
         }
 
-        Chip8Engine {
+        Ok(Chip8Engine {
             rom: rom.to_vec(),
             program_counter: 0x200,
             memory,
@@ -136,108 +124,119 @@ impl Chip8Engine {
             display: Display::new(),
             keyboard: 0,
             stack: Vec::new(), // TODO(ben): sized vec?
-        }
+        })
     }
 
-    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
-        for _ in 0..240 {
-            self.tick()?;
-        }
+    pub fn _run() {
+        todo!(
+            "Run will handle various operations, from reading flags that user can provide.
+            These flags will primarily control what an operation might do, how fast the game
+            engine will run, debug controls, etc.
 
-        Ok(())
+            Run will also initialize various I/O devices such as keyboard control, sounds, display
+            and manage the connections with those devices efficiently."
+        );
     }
 
-    /// tick executes one operation at a time.
+    // Tick handles the fetch/decode/execute loop for a single operation.
     fn tick(&mut self) -> Result<(), Box<dyn Error>> {
-        let code = self.fetch();
-        println!("CODE: {:#02x}", code);
-        let instruction = self.decode(code).ok_or_else(|| {
-            println!("OUTPUT: {:?}", self.display.screen);
+        let code = {
+            let slice = [
+                self.memory[self.program_counter],
+                self.memory[self.program_counter + 1],
+            ];
 
-            "unknown instruction"
-        })?;
-        self.execute(instruction)?;
+            self.program_counter += 2;
 
-        Ok(())
-    }
+            u16::from_be_bytes(slice)
+        };
 
-    /// fetch grabs the next two instructions, returning a u16.
-    fn fetch(&mut self) -> u16 {
-        let slice = [
-            self.memory[self.program_counter],
-            self.memory[self.program_counter + 1],
-        ];
+        let opcode = Opcode {
+            x: (code & 0x0100 >> 2) as u8,
+            y: (code & 0x0011 >> 1) as u8,
+            n: (code & 0x0001) as u8,
+            kk: (code & 0x0011) as u8,
+            addr: (code & 0x0111) as u16,
+        };
 
-        self.program_counter += 2;
-
-        u16::from_be_bytes(slice)
-    }
-
-    fn decode(&self, code: u16) -> Option<Opcode> {
-        // TODO(ben): can decoding fail? Likely, yes. Should
-        // error with Result type.
         match code & 0xF000 {
             0x0000 => match code & 0x00F0 {
-                0x00E0 => Some(create_opcode(code, Instructions::Clear)),
-                _ => None,
+                // 0x00E0 - CLS
+                0x00E0 => {
+                    for pixel in self.display.iter_mut() {
+                        *pixel = 0x0;
+                    }
+                }
+                _ => todo!(),
             },
-            0x1000 => Some(create_opcode(code, Instructions::Jump)),
-            0x6000 => Some(create_opcode(code, Instructions::SetVX)),
-            0x7000 => Some(create_opcode(code, Instructions::AddVX)),
-            0xA000 => Some(create_opcode(code, Instructions::SetI)),
-            0xD000 => Some(create_opcode(code, Instructions::Draw)),
-            _ => None,
-        }
-    }
 
-    fn execute(&mut self, opcode: Opcode) -> Result<(), Box<dyn Error>> {
-        match opcode.instruction {
-            Instructions::Clear => {
-                for pixel in self.display.iter_mut() {
-                    *pixel = false;
+            // TODO(ben): why cast to usize? any alternatives?
+            // 0x1nnn - JP addr
+            0x1000 => self.program_counter = opcode.addr.into(),
+
+            // 0x6xnn - LD Vx, byte
+            0x6000 => self.registers[opcode.x as usize] = opcode.kk,
+
+            // 0x7xnn - ADD Vx, byte
+            0x7000 => self.registers[opcode.x as usize] += opcode.kk,
+
+            // 0xAnnn - LD I, addr
+            0xA000 => self.index_register = opcode.addr,
+
+            // 0xDxyn - DRW Vx, Vy, nibble
+            //
+            // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+            // The interpreter reads n bytes from memory, starting at the address stored in I.
+            // These bytes are then displayed as sprites on screen at coordinates (Vx, Vy).
+            // Sprites are XORed onto the existing screen.
+            // If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0.
+            // If the sprite is positioned so part of it is outside the coordinates of the display,
+            // it wraps around to the opposite side of the screen.
+            0xD000 => {
+                // TODO(ben): get a hold of your numeric casting.
+                let x = self.registers[opcode.x as usize] % self.display.width as u8;
+                let y = self.registers[opcode.y as usize] % self.display.height as u8;
+
+                self.registers[0xF] = 0;
+
+                let start = self.index_register as usize;
+                for i in 0..opcode.n {
+                    let read = self.memory[start + i as usize];
+                    let curr_x = x.into();
+                    let curr_y = (y + i) as usize;
+                    let current_pixel = self.display.get_pixel(curr_x, curr_y).unwrap_or(0);
+
+                    let pixel_to_draw = read ^ current_pixel;
+                    self.display
+                        .write_pixel(curr_x, curr_y, pixel_to_draw)
+                        .unwrap_or(0);
+
+                    // Collision.
+                    //
+                    // A ^ B will turn a bit to zero if and only if
+                    // A and B both have a bit that was already. therefore
+                    // a collision must have occured if A & B is not zero.
+                    if read & current_pixel != 0 {
+                        self.registers[0xF] = 1;
+                    }
                 }
             }
-            Instructions::Jump => self.program_counter = opcode.nnn.into(),
-            // TODO(ben): why cast to usize? any alternatives?
-            Instructions::SetVX => self.registers[opcode.x as usize] = opcode.nn,
-            Instructions::AddVX => self.registers[opcode.x as usize] += opcode.nn,
-            Instructions::SetI => self.index_register = opcode.nnn,
-            Instructions::Draw => {
-                todo!()
-            }
-        }
+            _ => return Err("unknown instruction".into()),
+        };
 
         Ok(())
     }
 }
 
-// TODO(ben): should this be within the Chip-8 Engine struct?
-fn create_opcode(code: u16, instruction: Instructions) -> Opcode {
-    Opcode {
-        x: (code & 0x0100 >> 2) as u8,
-        y: (code & 0x0011 >> 1) as u8,
-        n: (code & 0x0001) as u8,
-        nn: (code & 0x0011) as u8,
-        nnn: (code & 0x0111) as u16,
-        instruction,
-    }
-}
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_name = "./src/IBM Logo.ch8";
+    let mut chip_8 = Chip8Engine::new(file_name)?;
 
-const FONT_SET: [i32; 80] = [
-    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-    0x20, 0x60, 0x20, 0x20, 0x70, // 1
-    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
-];
+    for _ in 0..200 {
+        chip_8.tick()?;
+    }
+
+    println!("{:?}", chip_8.display);
+
+    Ok(())
+}
